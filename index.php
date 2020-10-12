8ee9ddb30a220fa26385ea8c7e497e8db9fcb1bd
Pedro Sérgio <www.psro@gmail.com>
2020-10-11 22:27:17 -0300

    routes

--- index.php
+++ index.php
@@ -3,15 +3,44 @@
 require_once './config/config.php';
 
 Loader::include('Router');
+Loader::include('Session');
+Loader::include('Utility');
 
 $routes = new Router;
 
+// server root
 $routes->bind('', 'app');
-$routes->bind('app');
+$routes->bind('bookmarks', 'app');
 
-$routes->bind('login');
-$routes->bind('register');
-$routes->bind('leave');
-$routes->bind('logout');
+// bind routes
+$routes->bindall([
+    // login/register routes
+    'login',
+    'register',
+    // app actions routes
+    'app',
+    'new_bookmark',
+    'logout',
+    'delete',
+    'settings',
+    'users',
+    // app confirmation routes
+    'leave',
+    'confirm_delete',
+]);
+
+$routes->protect(!Session::user(), [
+    '',
+    'app',
+    'bookmarks',
+    'new_bookmark',
+    'logout',
+    'delete',
+    'settings',
+    'leave',
+    'confirm_leave'
+], function() {
+    view('denied');
+});
 
 $routes->dispatch();
